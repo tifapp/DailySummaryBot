cace65fd08fe3367943644af60923c141995f9b3
tifapp <tifapppublic@gmail.com>
2024-04-16 14:11:04 -0700

    Fix test to align with slack request Add error log

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ mod github;
 mod slack;
 mod s3;
 
-use lambda_http::{run, service_fn, tracing::{self, error}, Body, Request, Response};
+use lambda_http::{run, service_fn, tracing::{self}, Body, Request, Response};
 use anyhow::{Result, anyhow, Context};
 use reqwest::StatusCode;
 use crate::slack::{verify_slack_request, SlackRequestBody};
 use crate::sprint_summary::{create_sprint_message};
-use tracing::info;
+use tracing::{error, info};
 use serde::de::DeserializeOwned;
 
 //handle triggers
@@ -72,6 +72,8 @@ async fn function_handler(event: Request) -> Result<Response<Body>, lambda_http:
             Ok(resp)
         },
         Err(e) => {
+            error!("Sending error: {:?}", e);
+
             Ok(Response::builder()
                 .status(StatusCode::BAD_REQUEST)
                 .body(Body::from(format!("Error processing command: {}", e)))
@@ -92,14 +94,55 @@ mod tests {
     use std::{env, collections::HashMap};
     use super::*;
     use lambda_http::{http::StatusCode, Body::Text, RequestExt};
+    use reqwest::Method;
     
     #[tokio::test]
     async fn test_function_handler() {
         env::set_var("RUST_LOG", "debug");
         let _ = env_logger::try_init();
         dotenv::dotenv().ok();
+        let _ = env_logger::builder().is_test(true).try_init();
 
-        let request = Request::new(lambda_http::Body::Empty).with_query_string_parameters(vec![("name", "Tester")].iter().map(|&(k, v)| (k.to_string(), v.to_string())).collect::<HashMap<String, String>>());
+        // Simulate incoming HTTP POST request data
+        let headers = [
+            ("content-length", "447"),
+            ("x-amzn-tls-version", "TLSv1.3"),
+            ("x-forwarded-proto", "https"),
+            ("x-forwarded-port", "443"),
+            ("x-forwarded-for", "54.209.26.217"),
+            ("accept", "application/json,*/*"),
+            ("x-amzn-tls-cipher-suite", "TLS_AES_128_GCM_SHA256"),
+            ("x-amzn-trace-id", "Root=1-661e3b2e-1c1e0d877d8dcc312dd57823;Parent=7810b7bb797c88b8;Sampled=0;Lineage=f8c01797:0"),
+            ("host", "6zyr7rber5tcwxhf5ji5f266hi0sgvuh.lambda-url.us-west-2.on.aws"),
+            ("content-type", "application/x-www-form-urlencoded"),
+            ("x-slack-request-timestamp", "1713257262"),
+            ("x-slack-signature", "v0=caef35c6b7296dcf7fbe4b1ea0fba1cdf7b24653f27bb3ef719b2c64e76e6bad"),
+            ("accept-encoding", "gzip,deflate"),
+            ("user-agent", "Slackbot 1.0 (+https://api.slack.com/robots)"),
+        ];
+
+        let body = serde_urlencoded::to_string([
+            ("token", "JxPzi5d87tmKzBvRUAyE9bIX"),
+            ("team_id", "T01BFE465AN"),
+            ("team_domain", "tif-corp"),
+            ("channel_id", "C06RRR7NBAB"),
+            ("channel_name", "daily-summary"),
+            ("user_id", "U01CL8PLU72"),
+            ("user_name", "seanim0920"),
+            ("command", "/sprint-kickoff"),
+            ("text", "09/20/2025 test"),
+            ("api_app_id", "A0527UHK2F3"),
+            ("is_enterprise_install", "false"),
+            ("response_url", "https://hooks.slack.com/commands/T01BFE465AN/6981624816737/DjjVeStvAKmQlbIXoPvXymce"),
+            ("trigger_id", "696604658574949.1389480209362.6f38e7650555ab0cce5dc915f2c4fb1a0")
+        ]).expect("Failed to serialize test body");
+
+        let mut request = Request::new(body.into());
+        *request.method_mut() = Method::POST;
+        for (header_name, header_value) in headers.iter() {
+            request.headers_mut().insert(*header_name, header_value.parse().unwrap());
+        }
+
         let expected_response = "success";
 
         match function_handler(request).await {

cb6448aae8dc2d4e5e0b9848149dc299d43251f0
tifapp <tifapppublic@gmail.com>
2024-04-27 01:12:51 -0700

    no message

--- src/utils/eventbridge.rs
+++ src/utils/eventbridge.rs
@@ -43,6 +43,8 @@ impl EventBridgeExtensions for Client {
     async fn delete_daily_trigger_rule(&self, rule_name: &str) -> Result<()> {
         info!("{:?}", self.describe_rule().name(rule_name).send().await.map_err(|e| anyhow!("Failed to delete rule: {}", e))?);
 
+        self.remove_targets().rule(rule_name).send().await?;
+
         self.delete_rule().name(rule_name).send().await.map_err(|e| anyhow!("Failed to delete rule: {}", e))?;
 
         Ok(())
